105a60a8978a275585c9510ddc9055e148b7ff2a
GARUSSB <sparkgabo94@gmail.com>
2016-02-15 11:48:32 -0430

    Update main.php

--- controllers/main.php
+++ controllers/main.php
@@ -11,12 +11,33 @@ if (!isset($_SESSION['user'])) {
  ?>
 <html>
 <head>
+	<title></title>
+	<meta lang="es">
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styleAR.css" />
 </head>
 <body>
-	<p>
-		You are now logged in
-		...this is the main.
-		<a href="index.php?op=logout">Logout</a>
-	</p>
+	<br>
+	<header>
+		<hgroup>
+			<h1><img id="headimg" src="bar_gob2.png"><img src="logotrans2.png" width="60px" height="75px"></h1> <br>
+		</hgroup>
+	</header>
+	<br>
+	<br>
+	<div class="flex-container">
+  		<div class="flex-item">flex item 1</div>
+  		<div class="flex-item">flex item 2</div>
+  		<div class="flex-item">flex item 3</div> 
+  		<div class="flex-item">flex item 4</div>  
+	</div>
+
+	<br>
+
+	<div class="logoutButton">
+	<a class="logout" href="index.php?op=logout">Desconectarse</a>
+	</div>
+
+	<footer>Diseño por Gabriel Rivero. 2016</footer>
 </body>
 </html>
